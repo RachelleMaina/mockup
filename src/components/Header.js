537ebb1ef4e147c504e6a777198e0556c02c67a7
Rachel Maina <rachel.maina@andela.com>
2019-04-05 14:05:41 +0300

    Added login

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,10 +11,11 @@ const Header = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
+        <p className="search-icon"><i className="fas fa-search"></i></p>
           <NavLink to="/login" className="nav-link">
             Login
           </NavLink>
-          <Nav.Link className="nav-link get-started">Get Started</Nav.Link>
+          <NavLink className="nav-link get-started">Get Started</NavLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
